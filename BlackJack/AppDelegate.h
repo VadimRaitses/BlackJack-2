73507218c22ddd36a07c281fcc3bfd58c60d56f9
Vadim Raitses <AngelDust@MAC.local>
2014-08-19 22:19:51 +0300

    Initial Commit

---
+++ BlackJack/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BlackJack
+//
+//  Created by Vadim Raitses on 8/19/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
